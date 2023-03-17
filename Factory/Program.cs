630c21bf1d47334541fbb2ebb36db1a9269b7d2b
Justin Lee <lee.justin001126@gmail.com>
2023-03-17 09:00:46 -0700

    working on migrations WIP

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Registrar.Models;
+using Factory.Models;
 
 namespace Registrar
 {
