2f42a19c907c54322280986da1c54548da2ca17b
Justin Lee <lee.justin001126@gmail.com>
2023-03-17 13:54:15 -0700

    Engineer Controller WIP

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -26,5 +26,33 @@ namespace Factory.Controllers
       ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineDetails");
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      if (engineer.MachineId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers
+        .Include(engineer => engineer.Machine)
+        .Include(engineer => engineer.JoinEntities)
+        .ThenInclude(join => join.Liscence)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Details")
+    }
   }
 }
