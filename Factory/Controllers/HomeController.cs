ba00dba3f15e94ca8dd1d4601726f181960d81b4
Justin Lee <lee.justin001126@gmail.com>
2023-03-19 09:22:40 -0700

    Realized that I probably did not need 3 models for this project and could have just used 2 models to compare each other WIP

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -7,10 +7,22 @@ namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      License[] theseLicenses = _db.Licenses.ToArray();
+      Engineer[] theseEngineers = _db.Engineers.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("licenses", theseLicenses);
+      model.Add("engineers", theseEngineers);
+      return View(model);
     }
   }
 }
